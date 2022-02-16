5c72fb6868667397a088328a48a462569849ea82
mwl-plus <mwl930@qq.com>
2022-02-16 10:18:13 +0800

    git提交

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
 import React from 'react'
 import './App.scss'
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom'
+import Login from '@/pages/Login'
+import Layout from '@/pages/Layout'
 const App = () => {
   return (
-    <div className='app'>App</div>
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => {
+              return <Redirect to="/home" />
+            }}
+          ></Route>
+
+          <Route path="/login" component={Login}></Route>
+          <Route path="/home" component={Layout}></Route>
+        </Switch>
+      </Router>
+    </div>
   )
 }
 
---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,12 @@
+import { Button } from 'antd-mobile'
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+      登录页面 <Button color="success">Success</Button>
+    </div>
+  )
+}
+
+export default index
